44b59fdf5142e09e664dff120738ec328ea78331
THIOMBIANO Yempabou Romaric Johan <romaricthiombiano4@gmail.com>
2026-09-22 20:30:37 +0000

    fix: applique la remarque de review (renommage variable)

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
-function login(u, p) {
-  const d = { utilisateur: u, motDePasse: p };
-  console.log("Tentative de connexion de : " + d.utilisateur);
-  return d;
+function login(nomUtilisateur, motDePasse) {
+  const identifiants = { utilisateur: nomUtilisateur, motDePasse: motDePasse };
+  console.log("Tentative de connexion de : " + identifiants.utilisateur);
+  return identifiants;
 }
